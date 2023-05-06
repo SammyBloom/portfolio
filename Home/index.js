6fe63c962954cbd8b9dcbea2a1deafa50fa939e1
SammyBloom <nwachiemi@gmail.com>
2023-05-06 12:43:04 +0100

    working on making the site responsive and also started calling the weather api

---
+++ Home/index.js
@@ -0,0 +1,25 @@
+/*
+    Weather API call for Lagos, Nigeria using fetch()
+*/
+const fetch = require('node-fetch');
+
+const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Lagos';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '49d0fcef32mshd7c75c54b9ff7fap152f81jsn2f10f04c9a2a',
+    'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+  }
+};
+
+async function getapi(){
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getapi();
