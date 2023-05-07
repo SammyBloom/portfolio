2eeceea2d3b1172d17d571feee5a7fffed786f79
SammyBloom <nwachiemi@gmail.com>
2023-05-07 16:26:13 +0100

    successfully called the api and displayed the data in the browser, for the call api section. What is left is haaders, styling and responsiveness

--- Home/index.js
+++ Home/index.js
@@ -2,7 +2,7 @@
     Weather API call for Lagos, Nigeria using fetch()
 */
 
-const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Lagos';
+const url = "https://weatherapi-com.p.rapidapi.com/current.json?q=Lagos";
 const options = {
   method: 'GET',
   headers: {
@@ -14,11 +14,32 @@ const options = {
 async function getapi(){
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
+        const result = await response.json();
+
+        const cityEl = document.getElementById("city");
+        const country = document.getElementById("country");
+        const latitude = document.getElementById("lat");
+        const longitude = document.getElementById("long");
+        const temperature = document.getElementById("temp");
+        const temperature_feel = document.getElementById("temp-feel");
+        const todayDate = document.getElementById("date");
+        const city2 = document.getElementById("city2");
+        
+        cityEl.innerHTML = result.location.name;
+        country.innerHTML = result.location.country;
+        latitude.innerHTML = result.location.lat;
+        longitude.innerHTML = result.location.lon;
+        temperature.innerHTML = result.current.temp_c;
+        temperature_feel.innerHTML = result.current.feelslike_c;
+        todayDate.innerHTML = result.location.localtime;
+        city2.innerHTML = result.location.name;
+
     } catch (error) {
         console.error(error);
     }
+    
 }
 
 getapi();
+
+
