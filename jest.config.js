87e0168edd091b9132828772970baf5047da4db5
Samuel Nwanwobi <nwachiemi@gmail.com>
2025-06-02 21:38:08 +0100

    removed the test line of code in deploy.yml

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./res/script/index.test.js'],
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  testEnvironment: 'jsdom',
 };
+
